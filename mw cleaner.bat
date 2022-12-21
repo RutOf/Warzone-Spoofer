452c1714eecce7cb0a53670c8c02eb046266ba16
Slackes <FnoberzOfficial@pm.me>
2022-12-21 07:12:48 +0700

    Update mw cleaner.bat

--- mw cleaner.bat
+++ mw cleaner.bat
@@ -6,16 +6,15 @@ title Tuga's COD Tracer Cleaner
 
 REM --> Check for permissions
 IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+    >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
 ) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+    >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
- 
 
 REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
-echo Requesting administrative privileges...
-goto UACPrompt
+    echo Requesting administrative privileges...
+    goto UACPrompt
 ) else ( goto gotAdmin )
 
 :UACPrompt
@@ -23,6 +22,22 @@ echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 set params= %*
 echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
+"%temp%\getadmin.vbs"
+del "%temp%\getadmin.vbs"
+exit /B
+
+:gotAdmin
+pushd "%CD%"
+CD /D "%~dp0"
+:--------------------------------------
+cls
+echo Cleaning...
+timout 2
+cls
+
+REM --> Kill processes that may interfere with the deletion of files
+taskkill /IM Agent.exe /F
+taskkill /IM Battle.net.exe /F
 "%temp%\getadmin.vbs"
 del "%temp%\getadmin.vbs"
 exit /B
