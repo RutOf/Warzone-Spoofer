8110040bc0a44a480ada468ca7a2df5c10530c0a
Serups <FnoberzOfficial@pm.me>
2023-02-12 02:33:05 +0700

    Here is an improved version of the function that includes some added comments and error handling:

--- Source/driver.cpp
+++ Source/driver.cpp
@@ -16,20 +16,38 @@ NTSTATUS Driver::InitHook()
     RtlInitUnicodeString(&driverName, L"\\Driver\\disk");
     PDRIVER_OBJECT pDiskDriverObject = nullptr;
 
+    // Find the disk driver object
     status = Utility::FindDriverObject(&pDiskDriverObject, &driverName);
+
+    // Check if the disk driver object was found
     if (!NT_SUCCESS(status) || !pDiskDriverObject)
     {
+        // If not, log an error and return an unsuccessful status
         print_error_message("Could not find disk driver object. NTSTATUS: 0x%08X", status);
         return STATUS_UNSUCCESSFUL;
     }
 
+    // Log success message indicating the disk driver object was found
     print_success_message("Found disk driver object: %p", pDiskDriverObject);
 
+    // Store the original dispatch function of IRP_MJ_DEVICE_CONTROL
     HookHandler::OriginalDispatch = pDiskDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL];
+
+    // Check if the dispatch function is valid
+    if (!HookHandler::OriginalDispatch)
+    {
+        // If not, log an error and return an unsuccessful status
+        print_error_message("Could not retrieve original dispatch function.");
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    // Replace the IRP_MJ_DEVICE_CONTROL dispatch function with the hooked function
     pDiskDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = HookHandler::HookedDeviceControl;
 
+    // Log success messages indicating the original dispatch was stored and the device control dispatch was swapped
     print_success_message("Stored original dispatch: %p", HookHandler::OriginalDispatch);
     print_success_message("Swapped device control dispatch: %p -> %p", HookHandler::OriginalDispatch, HookHandler::HookedDeviceControl);
 
+    // Return a successful status
     return STATUS_SUCCESS;
 }
