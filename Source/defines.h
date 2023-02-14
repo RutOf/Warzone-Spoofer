c8c703b747458d8fcf2e688a5e32ce0bf0e3ee93
Serups <FnoberzOfficial@pm.me>
2023-02-14 15:56:27 +0700

    Provide more meaningful names for the macros and variables that describe their functionality.

--- Source/defines.h
+++ Source/defines.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #ifdef _DEBUG
-#define print_message(prefix, ...) DbgPrintEx(0, 0, "[" prefix "] " __VA_ARGS__)
+#define DEBUG_PRINT(prefix, ...) DbgPrintEx(0, 0, "[" prefix "] " __VA_ARGS__)
 #else
-#define print_message(prefix, ...) 
+#define DEBUG_PRINT(prefix, ...) 
 #endif
 
-#define print_success_message(...) print_message("+", __VA_ARGS__)
-#define print_error_message(...) print_message("-", __VA_ARGS__)
+#define SUCCESS_PRINT(...) DEBUG_PRINT("+", __VA_ARGS__)
+#define ERROR_PRINT(...) DEBUG_PRINT("-", __VA_ARGS__)
