ddb40d49c0bbcea3a6ca35899a796541ab0db430
Serups <FnoberzOfficial@pm.me>
2023-03-06 15:57:35 +0700

    Retrieve driver object name.

--- Source/utility.cpp
+++ Source/utility.cpp
@@ -22,26 +22,85 @@ if (!NT_SUCCESS(status))
     return status;
 }
 
-ZwClose(dirHandle);
-
-
-UNICODE_STRING objectName = {};
-RtlInitUnicodeString(&objectName, DriverName->Buffer);
-
-PVOID driverObject = nullptr;
-status = ObOpenObjectByName(&objAttribs, dir, &objectName, 0, nullptr, KernelMode, &driverObject);
-if (!NT_SUCCESS(status))
+NTSTATUS GetDriverObjectByName(const UNICODE_STRING* DriverName, PDRIVER_OBJECT* DriverObject)
 {
-    ObDereferenceObject(dir);
-    ZwClose(dirHandle);
+    // Check input parameters
+    if (!DriverName || !DriverName->Buffer || !DriverObject)
+    {
+        return STATUS_INVALID_PARAMETER;
+    }
+
+    NTSTATUS status = STATUS_SUCCESS;
+    HANDLE dirHandle = nullptr;
+    OBJECT_ATTRIBUTES objAttribs = {};
+    UNICODE_STRING dirPath = RTL_CONSTANT_STRING(L"\\Driver");
+
+    InitializeObjectAttributes(&objAttribs, &dirPath, OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE, nullptr, nullptr);
+
+    // Open the driver directory
+    IO_STATUS_BLOCK ioStatus = {};
+    status = ZwCreateFile(&dirHandle, FILE_GENERIC_READ, &objAttribs, &ioStatus, nullptr, FILE_ATTRIBUTE_DIRECTORY, 0, FILE_OPEN, FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT, nullptr, 0);
+    if (!NT_SUCCESS(status))
+    {
+        return status;
+    }
+
+    // Enumerate the directory to find the target driver object
+    BYTE buffer[sizeof(FILE_DIRECTORY_INFORMATION) + MAX_PATH * sizeof(WCHAR)];
+    PFILE_DIRECTORY_INFORMATION dirInfo = reinterpret_cast<PFILE_DIRECTORY_INFORMATION>(buffer);
+    ULONG context = 0;
+    BOOLEAN found = FALSE;
+
+    while (TRUE)
+    {
+        status = ZwQueryDirectoryFile(dirHandle, nullptr, nullptr, nullptr, &ioStatus, dirInfo, sizeof(buffer), FileDirectoryInformation, TRUE, nullptr, FALSE);
+        if (!NT_SUCCESS(status))
+        {
+            break;
+        }
+
+        if (RtlCompareUnicodeString(DriverName, &dirInfo->FileName, TRUE) == 0)
+        {
+            found = TRUE;
+            break;
+        }
+
+        if (dirInfo->NextEntryOffset == 0)
+        {
+            break;
+        }
+
+        dirInfo = reinterpret_cast<PFILE_DIRECTORY_INFORMATION>(reinterpret_cast<PUCHAR>(dirInfo) + dirInfo->NextEntryOffset);
+    }
+
+    if (!found)
+    {
+        status = STATUS_NOT_FOUND;
+        goto Cleanup;
+    }
+
+    // Open the driver object
+    UNICODE_STRING objectName = {};
+    RtlInitUnicodeString(&objectName, dirInfo->FileName.Buffer);
+
+    PVOID driverObject = nullptr;
+    status = ObOpenObjectByName(&objAttribs, nullptr, &objectName, nullptr, nullptr, OBJ_KERNEL_HANDLE, nullptr, &driverObject);
+    if (!NT_SUCCESS(status))
+    {
+        goto Cleanup;
+    }
+
+    *DriverObject = static_cast<PDRIVER_OBJECT>(driverObject);
+
+Cleanup:
+    if (dirHandle)
+    {
+        ZwClose(dirHandle);
+    }
+
     return status;
 }
 
-*DriverObject = (PDRIVER_OBJECT)driverObject;
-ObDereferenceObject(dir);
-ZwClose(dirHandle);
-return status;
-}
 
 NTSTATUS FindDriverObjectByName(const UNICODE_STRING* DriverName, PDRIVER_OBJECT* DriverObject)
 {
